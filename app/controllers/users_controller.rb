dc0b35be71a7d542515fd2a0df8dd05e953741cc
EC2 Default User <ec2-user@ip-172-31-10-92.us-east-2.compute.internal>
2018-03-22 13:42:31 +0000

    Display Microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,11 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def index
-   @users = User.paginate(page: params[:page])
+    @users = User.paginate(page: params[:page])
   end
   
   def destroy
